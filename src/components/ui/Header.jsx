4e7a159c3c98c75ce29e97180734f3232416f738
Che0000007 <cjaponte@campoalto.edu.co>
2026-02-26 11:46:45 -0500

    chore: cambio menor en el uso header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -12,25 +12,35 @@ export const Header = () => {
     return (
         <>
             <header
-                className="fixed left-1/2 -translate-x-1/2 flex items-center justify-center sm:justify-between align-middle
+                className="fixed left-1/2 -translate-x-1/2 flex items-center justify-between sm:justify-between align-middle
                 p-2 sm:px-5 font-semibold text-slate-600 max-[980px]:text-xs min-[1000px]:text-base h-[15%] max-h-12 top-2 w-[85%] max-w-300 rounded-xl z-20
                 bg-background-light/50 backdrop-blur-md shadow-lg shadow-medium/30 border border-b-8 border-slate-300
                 dark:bg-background-dark/70 dark:text-slate-400 dark:border-slate-800 dark:shadow-medium/15"
             >
                 <section
-                    className="hidden group relative sm:flex items-center justify-center align-middle gap-2"
+                    className="group relative flex items-center justify-center align-middle gap-2"
                 >
                     <Cog className="size-6 group-hover:rotate-180 transition-all transform duration-500" />
                     <span>Christopher Aponte</span>
 
-                    <article className="hidden group-hover:block absolute max-w-[50dvh] top-full -left-5 max-h-[50dvh] overflow-y-auto z-10">
-                        <div className="w-full p-3 bg-red-500">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident accusamus possimus quos amet accusantium corporis, quidem similique hic, fugiat maiores animi praesentium harum? Excepturi exercitationem eligendi provident delectus aliquam iusto!
+                    <article className="absolute top-full left-0 w-full max-w-[50dvh] -z-10 opacity-0 -translate-y-3 pointer-events-none group-hover:opacity-100 group-hover:translate-y-0 group-hover:pointer-events-auto transition-all duration-300 ease-out"
+                    >
+                        <div className="flex flex-col mt-5 max-h-[25dvh] overflow-y-auto p-3">
+                            <div>
+                                <button className="border rounded-lg p-2">Prueba</button>
+                                <button className="border rounded-lg p-2">Prueba</button>
+                            </div>
+                            <button className="border rounded-lg p-2">Prueba</button>
+                            <button className="border rounded-lg p-2">Prueba</button>
+                            <button className="border rounded-lg p-2">Prueba</button>
+                            <button className="border rounded-lg p-2">Prueba</button>
+                            <button className="border rounded-lg p-2">Prueba</button>
+                            <button className="border rounded-lg p-2">Prueba</button>
                         </div>
                     </article>
                 </section>
                 <section
-                    className="flex space-x-4"
+                    className="hidden sm:flex space-x-4"
                 >
                     <a href="#" className="group relative">
                         About
